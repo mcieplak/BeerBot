af16b863e8774571a6e1e6445c614f71c54a2f18
Jesus Horta <jhorta@ucsd.edu>
2015-03-03 22:46:27 -0800

    Somewhat working shirt detection

--- src/facerec.cpp
+++ src/facerec.cpp
@@ -28,26 +28,64 @@ CascadeClassifier eyes_cascade;
 string window_name = "Capture - Face detection";
 RNG rng(12345);
 
+void drawMap( const Mat& current, Mat& result, int step, double threshold) {
+  Mat blue, green, red;
+  int blueC = 0, greenC = 0, redC = 0;
+  for(int y = 0; y < result.rows; y += step)
+    for(int x = 0; x < result.cols; x += step)
+    {
+      //const Point2f& fxy = current.at<Point2f>(y, x);
+      Vec3b pixel = current.at<Vec3b>(y, x);
+      if( pixel[2] > 110 && pixel[1] < 160 && pixel[0] < 50 ){
+        blue.at<uchar>(Point(x,y)) = 255;
+        blueC++;
+      }
+      else {
+        blue.at<uchar>(Point(x,y)) = 0;
+      }
+
+    }
+  if( blueC > greenC ) {
+    if( blueC > redC )
+      result = blue;
+    else
+      result = red;
+  }
+  else {
+    if( greenC > redC )
+      result = green;
+    else 
+      result = red;
+  }
+}
+
 void faceDetect( const sensor_msgs::Image::ConstPtr & msg ) {
   cv_bridge::CvImagePtr cv_ptr;
 
   try
   {
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+    cv_ptr = cv_bridge::toCvCopy(msg, "rgb8");
   }
   catch (cv_bridge::Exception & e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
   current = cv_ptr->image;
+  
+  if(prev.data) {
+    drawMap(current, cv_ptr->image, 1, 255);
+  }
+  prev = current;
+  image_pub.publish(cv_ptr->toImageMsg());
 
 }
 
 int main( int argc, char ** argv ) {
   ros::init(argc, argv, "facerec");
+  prev = 0;
   //-- 1. Load the cascades
-  if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-  if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+  // if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+  // if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
   ros::NodeHandle node;
   image_transport::ImageTransport it(node);
   image_transport::Subscriber mySub = it.subscribe("/camera/visible/image", 
